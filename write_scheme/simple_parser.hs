ed3f98bf2931523535635483816ca17e2a2543b6
tsudodog <malamute29@gmail.com>
2018-01-30 20:05:00 -0600

    this is the "Write Yourself a Scheme in 48 Hours/Parsing" Tutorial!

---
+++ write_scheme/simple_parser.hs
@@ -0,0 +1,16 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_!"
+
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found Value " ++ show val 
+
+main :: IO ()
+main = do
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
