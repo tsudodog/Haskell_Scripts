a4ec21bd5f5dda37f5e1c093e119ef3bf9280abc
tsudodog <malamute29@gmail.com>
2018-01-31 20:43:13 -0600

    kana for codewars

--- scrapfiles/hello.hs
+++ scrapfiles/hello.hs
@@ -1,2 +1,16 @@
-module Main where
-main = putStrLn "Hello World"
+
+
+
+repeatStr :: Int -> String -> String
+repeatStr n str = [1..n] >> str
+
+
+repeatStrAlt :: Int -> String -> String
+repeatStrAlt n str = concat $ replicate n str
+
+
+arrayDiff :: Eq a => [a] -> [a] -> [a]
+arrayDiff xs ys = [ x | x <- ys, x `notElem` xs ]
+
+difference :: Eq a => [a] -> [a] -> [a]
+difference a b = filter (`notElem` b) a
